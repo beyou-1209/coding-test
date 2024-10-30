49bee2ee37d71a8e780669d9f8b472fc543fa221
Ahn serim <srahn0101@gmail.com>
2024-10-30 20:37:59 +0900

    줄바꿈 수정

--- python/241030/두수의 연산값 비교하기.py
+++ python/241030/두수의 연산값 비교하기.py
@@ -6,6 +6,7 @@
 양의 정수 a와 b가 주어졌을 때, a ⊕ b와 2 * a * b 중 더 큰 값을 return하는 solution 함수를 완성해 주세요.
 단, a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 합니다.
 """
+
 def solution(a, b):
     n1 = int(str(a) + str(b))
     n2 = 2*a*b
--- python/241030/조건 문자열.py
+++ python/241030/조건 문자열.py
@@ -10,6 +10,7 @@
 ineq는 "<"와 ">"중 하나고, eq는 "="와 "!"중 하나입니다.
 그리고 두 정수 n과 m이 주어질 때, n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 return하도록 solution 함수를 완성해주세요.
 """
+
 def solution(ineq, eq, n, m):
     if ((n >= m) and (ineq == ">")) or ((n <= m) and (ineq == "<")):
         return 1
