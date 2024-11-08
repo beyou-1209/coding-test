d42fa19f1b9897e5d2c3179130960676d741baec
Serim Ahn <srahn0101@gmail.com>
2024-11-08 13:28:54 +0900

    add

---
+++ C++/241108/2753.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int year = 0;
+    cin >> year;
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    {
+        cout << 1 << endl;
+    }
+
+    else
+    {
+        cout << 0 << endl;
+    }
+
+    return 0;
+}
